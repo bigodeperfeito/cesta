450dbe54e74f0507c649a50a76abec6f787c3c0d
bigodeperfeito <victor.galvao.pereira@escola.pr.gov.br>
2026-02-09 13:07:20 +0000

    1 aula

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import { Text, SafeAreaView } from 'react-native';
+import Sobrenome from './src/telas/Sobrenome';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>bigode perfeito</Text>
+      <Sobrenome nome='Marinho'/>
+    </SafeAreaView>
+  );
+}
+
